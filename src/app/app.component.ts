07af0d4430f760f312909eeddbef8a07d9456628
DJ Roychoudhury <djroychoudhury@gmail.com>
2022-05-30 23:13:30 -0600

    cleaned commented code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'comp-1';
-  // toggle:boolean =true;
-  // btnVal: string = "Add Trainee";
-
-  // btnClicked() {
-  //   this.toggle = !this.toggle;
-  //   if(this.btnVal == "Add Trainee"){
-  //     this.btnVal = "Submit Trainee";
-  //   }
-  //   else{
-  //     this.btnVal = "Add Trainee";
-  //   }
-  // }
 }
